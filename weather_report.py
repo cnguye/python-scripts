632a83533ed339c1162707eb3c08acbd2a629002
ceen <ngyn.christopher@gmail.com>
2022-06-06 09:41:47 -0700

    rewrote weather report to handle dictionary of locations.  More accurate readings for your friends!

--- weather_report.py
+++ weather_report.py
@@ -6,8 +6,16 @@ import os #provides ways to access the Operating System and allows us to read th
 
 load_dotenv()
 
-lat = '34.053939'
-lon = '-118.158953'
+locations = {
+    'Rosemead': {
+        'lat':'34.071922',
+        'lon':'-118.099458'
+    },
+    'East LA': {
+        'lat':'34.041316',
+        'lon':'-118.188220'
+    }
+}
 api_key = os.getenv("CURRENT_WEATHER")
 
 #telegram keys
@@ -15,32 +23,41 @@ telegram_chat_key = os.getenv("TELEGRAM_CHAT_KEY")
 #weather report API token
 wr_api_key = os.getenv("WR_STANDO")
 
-# connect to API
-weather_response = requests.get('https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units=imperial'.format(lat, lon, api_key))
-#print(weather_response.status_code)
+# start message
+message =   ("Mori mori mori mori!\n"
+            "Mori-oh Cho, radioooo!\n\n"
+            "Today's forcast is:\n")
 
-# get the data from API
-data = weather_response.text
+# loop for each location
+for location, coordinates in locations.items():
+    # connect to API
+    weather_response = requests.get('https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}&units=imperial'.format(coordinates['lat'], coordinates['lon'], api_key))
+    #print(weather_response.status_code)
 
-# parse the data into JSON format
-wr = json.loads(data)
+    # get the data from API
+    data = weather_response.text
 
-# print
-if(wr['cod'] != 200):
-    send_weather_report = requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text=Error {}: {}'.format(wr_api_key, telegram_chat_key, wr['cod'], wr['message']))
-else:
-    #format string
-    weather = wr['weather'][0]
-    main = wr['main']
-    temp = main['temp']
-    temp_min = main['temp_min']
-    temp_max = main['temp_max']
+    # parse the data into JSON format
+    wr = json.loads(data)
 
-    message =   ("Mori mori mori mori!\n"
-                "Mori-oh Cho, radioooo!\n\n"
-                "Today's forcast is:\n"
-                "{} <b>{}</b> ({})\n"
-                "Min: <i>{}</i>\n"
-                "Max: <i>{}</i>"
-                ).format(weather['description'], temp, main['feels_like'], temp_min, temp_max)
-    send_weather_report = requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&parse_mode=HTML&text={}'.format(wr_api_key, telegram_chat_key, message))
+    # print
+    if(wr['cod'] != 200):
+        # send_weather_report = requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&text=Error {}: {}'.format(wr_api_key, telegram_chat_key, wr['cod'], wr['message']))
+        message = message + "\n" + "Error fetching weather: " + wr['cod'] + ": " + wr['message']
+    else:
+        #format string
+        weather = wr['weather'][0]
+        main = wr['main']
+        temp = main['temp']
+        temp_min = main['temp_min']
+        temp_max = main['temp_max']
+
+        message = message + (
+                    "<b>{}</b>:\n"
+                    "{} \n"
+                    "Now: <b>{}</b> ({})\n"
+                    "Min: <i>{}</i>\n"
+                    "Max: <i>{}</i>\n\n"
+                    ).format(location, weather['description'], temp, main['feels_like'], temp_min, temp_max)
+    
+send_weather_report = requests.get('https://api.telegram.org/bot{}/sendMessage?chat_id={}&parse_mode=HTML&text={}'.format(wr_api_key, telegram_chat_key, message))
